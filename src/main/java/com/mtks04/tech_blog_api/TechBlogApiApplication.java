9c744eb91b8491a33b2ea5d3d7a9b82fca6bf432
MT-KS-04 <ktomis2004@gmail.com>
2026-03-08 00:14:12 +0700

    Initial commit: Add project files

---
+++ src/main/java/com/mtks04/tech_blog_api/TechBlogApiApplication.java
@@ -0,0 +1,12 @@
+package com.mtks04.tech_blog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechBlogApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mtks04/tech_blog_api/TechBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtks04.tech_blog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
