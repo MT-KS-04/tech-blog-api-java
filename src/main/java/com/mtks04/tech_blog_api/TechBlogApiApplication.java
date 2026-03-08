4e2c679cf2f55912f02c7db4a4b2ab1b78642110
MT-KS-04 <ktomis2004@gmail.com>
2026-03-08 22:46:07 +0700

    check server and setup api

--- src/main/java/com/mtks04/tech_blog_api/TechBlogApiApplication.java
+++ src/main/java/com/mtks04/tech_blog_api/TechBlogApiApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TechBlogApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TechBlogApiApplication.class, args);
+        SpringApplication.run(TechBlogApiApplication.class, args);
 	}
 }
